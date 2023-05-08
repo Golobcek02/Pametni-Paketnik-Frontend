c79720c81dadef0c7f0189e3c6201634b0691f71
PainOfExistance <matej113a@gmail.com>
2023-05-08 20:19:04 +0200

    improving jsx

--- src/Components/User/AddBox.jsx
+++ src/Components/User/AddBox.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 function AddBox() {
-    const [userID, setUserID] = useState('');
     const [smartBoxID, setSmartBoxID] = useState('');
 
     function submitAddBox() {
         axios.post("http://localhost:5551/addUserBox", {
-            user_id: userID,
+            user_id: Cookies.get("id"),
             smartbox_id: smartBoxID
         }).then(res => {
             if (res.status === 200) {
@@ -24,7 +24,6 @@ function AddBox() {
 
     return (
         <div>
-            <input type='text' id="user_id" onChange={(e) => { setUserID(e.target.value) }} placeholder="Enter User ID"></input>
             <input type='text' id="smartbox_id" onChange={(e) => { setSmartBoxID(e.target.value) }} placeholder="Enter Smartbox ID"></input>
             <button type='submit' onClick={submitAddBox}>Add Smartbox to User</button>
         </div>
