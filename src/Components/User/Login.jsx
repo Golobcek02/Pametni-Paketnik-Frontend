f3df6d3194a3af5d0271797a81b5cf92792d76c6
PainOfExistance <matej113a@gmail.com>
2023-05-08 18:07:51 +0200

    Implemented login view, added form to login and set coockies to username and id

--- src/Components/User/Login.jsx
+++ src/Components/User/Login.jsx
@@ -1,7 +1,36 @@
+import axios, { Axios } from 'axios';
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from 'react-router-dom';
+import Cookies from "js-cookie";
+
 function Login() {
-	return (
-		<>Login</>
-	)
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  function SubmitLogIn() {
+    axios.post("http://localhost:5551/login", {
+      Username: username,
+      Password: password
+    }).then(res => {
+      if (res.status == 200) {
+        console.log(res.data);
+        let expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 7);
+        Cookies.set('id', res.data.ID, { expires: expiryDate });
+        Cookies.set('username', res.data.Username, { expires: expiryDate });
+        navigate("/");
+      }
+    })
+  }
+
+  return (
+    <div>
+      <input type='text' id="username" onChange={(e) => { setUsername(e.target.value) }} placeholder="Enter Username"></input>
+      <input type='password' id="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Enter Password"></input>
+      <button type='submit' onClick={SubmitLogIn}>Post</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
