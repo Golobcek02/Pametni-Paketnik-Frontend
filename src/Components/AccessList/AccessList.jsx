700dd9ca2c2f58330b0d898020ace73529fda41d
Golobcek <klemen.golob77@gmail.com>
2023-05-17 18:13:49 +0200

    Added access list components and ready for list from backend

--- src/Components/AccessList/AccessList.jsx
+++ src/Components/AccessList/AccessList.jsx
@@ -1,49 +1,11 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-import Cookies from "js-cookie";
+import {AddedAccessList} from "./AddedAccessList";
+import AddAccess from "./AddAccess";
 
 export function AccessList() {
-    const [userToGrant, setUserToGrant] = useState('');
-    const [boxId, setBoxId] = useState('');
-    const [availableBoxes, setAvailableBoxes] = useState([]);
-
-
-    function addUserToAccessList() {
-        axios.post("http://localhost:5551/addAccessToUser", {
-            UserID: Cookies.get('id'),
-            AccessId: userToGrant,
-            BoxId: parseInt(boxId)
-        }).then((res) => {
-                console.log(res)
-            }
-        ).catch((err) => {
-            console.log(err)
-        })
-    }
-
-    useEffect(() => {
-        axios.get(`http://localhost:5551/getUserBoxes/${Cookies.get('id')}`).then((res) => {
-            console.log(res)
-            setAvailableBoxes(res.data)
-        }).catch(err => {
-            console.log(err)
-        })
-    }, [])
-
     return (
-        <div>
-            <input type='text' id="username" onChange={(e) => {
-                setUserToGrant(e.target.value)
-            }} placeholder="Enter User To Grant"></input>
-            <select id="box" onChange={(e) => {
-                setBoxId(e.target.value)
-            }}>
-                <option value="">Select Box</option>
-                {availableBoxes.map(option => (
-                    <option key={option.ID} value={option.BoxId}>{option.BoxId}</option>
-                ))}
-            </select>
-            <button type='submit' onClick={() => addUserToAccessList()}>Post</button>
-        </div>
+        <>
+            <AddAccess/>
+            <AddedAccessList/>
+        </>
     )
 }
---
+++ src/Components/AccessList/AddAccess.jsx
@@ -0,0 +1,51 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+function AddAccess() {
+    const [userToGrant, setUserToGrant] = useState('');
+    const [boxId, setBoxId] = useState('');
+    const [availableBoxes, setAvailableBoxes] = useState([]);
+
+
+    function addUserToAccessList() {
+        axios.post("http://localhost:5551/addAccessToUser", {
+            UserID: Cookies.get('id'),
+            AccessId: userToGrant,
+            BoxId: parseInt(boxId)
+        }).then((res) => {
+                console.log(res)
+            }
+        ).catch((err) => {
+            console.log("No user with that Username")
+        })
+    }
+
+    useEffect(() => {
+        axios.get(`http://localhost:5551/getUserBoxes/${Cookies.get('id')}`).then((res) => {
+            console.log(res)
+            setAvailableBoxes(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    return (
+        <div>
+            <input type='text' id="username" onChange={(e) => {
+                setUserToGrant(e.target.value)
+            }} placeholder="Enter User To Grant"></input>
+            <select id="box" onChange={(e) => {
+                setBoxId(e.target.value)
+            }}>
+                <option value="">Select Box</option>
+                {availableBoxes.map(option => (
+                    <option key={option.ID} value={option.BoxId}>{option.BoxId}</option>
+                ))}
+            </select>
+            <button type='submit' onClick={() => addUserToAccessList()}>Add Access</button>
+        </div>
+    )
+}
+
+export default AddAccess
