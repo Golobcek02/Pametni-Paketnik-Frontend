32a95fd26e10bb58c676d86b708dabd821461801
ReneRajzman <rene.rajzman@student.um.si>
2023-05-18 19:25:30 +0200

    Fetching orders works

--- src/Components/UserOrders/DisplayOrders.jsx
+++ src/Components/UserOrders/DisplayOrders.jsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import Cookies from "js-cookie";
 export function DisplayOrders() {
-    return (
-        <div>
-            <h1>Hello to this site!</h1>
-        </div>
-    );
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+
+        const userId = Cookies.get("id");
+        axios.get(`http://localhost:5551/getUserOrders/${userId}`)
+            .then((response) => {
+                console.log(response.data);
+                setOrders(response.data.orders);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+   
 }
