895721d4d288145e9ee4ce571902ffcd841854a8
Golobcek <klemen.golob77@gmail.com>
2023-05-18 12:47:09 +0200

    Added basic functionality to add orders to database

--- src/Components/PackageRoutes/PackageRoutes.jsx
+++ src/Components/PackageRoutes/PackageRoutes.jsx
@@ -3,10 +3,19 @@ import axios from "axios";
 
 export function PackageRoutes() {
     const [nonParsedRoutes, setNonParsedRoutes] = useState("");
+    const [boxId, setBoxId] = useState(0);
 
     function addRoutes() {
         const parsedRoutes = nonParsedRoutes.split(",").map(route => route.trim());
         axios.post("http://localhost:5551/addPackageRoute", {Stops: parsedRoutes}).then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    function addOrder() {
+        axios.post("http://localhost:5551/addOrder", {BoxId: parseInt(boxId), Status: "Pending"}).then((res) => {
             console.log(res)
         }).catch((err) => {
             console.log(err)
@@ -25,6 +34,19 @@ export function PackageRoutes() {
                     placeholder="Enter Routes Divided by ,"
                 />
                 <button type="submit" onClick={() => addRoutes()}>
+                    Post
+                </button>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    id="username"
+                    onChange={(e) => {
+                        setBoxId(parseInt(e.target.value));
+                    }}
+                    placeholder="Enter Package ID"
+                />
+                <button type="submit" onClick={() => addOrder()}>
                     Post
                 </button>
             </div>
